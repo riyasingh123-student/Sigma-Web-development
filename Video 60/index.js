2cf50cd14cc909179aca53ee9698173c7c0bad3d
riyasingh123-student <riyasingh790322@gmail.com>
2025-10-08 20:22:01 +0530

    Add files via upload

---
+++ Video 60/index.js
@@ -0,0 +1,26 @@
+console.log("This is string tutorial")
+let a = "Riya";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+// console.log(a[4])
+
+console.log(a.length)
+
+let real_name = "Riya"
+let friend = "Ankita"
+console.log("His name is " + real_name +  " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "CharanSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 4))
+console.log(b.slice(1))
+
+console.log(b.replace("Ch", "77"))
+console.log(b.concat(a, "Arora", "Chahal", "Supriya"))
+
+console.log(b)
