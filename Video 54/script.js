c301b109d65d0a6ba6124629c74d8408dd437686
riyasingh123-student <riyasingh790322@gmail.com>
2025-10-08 20:19:44 +0530

    Add files via upload

---
+++ Video 54/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to delete this page and blast burn your computer ")
+
+if (isTrue){
+    console.log("Computer is blasting")
+}
+
+else {
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+// document.body.style.backgroundColor = "yellow"
